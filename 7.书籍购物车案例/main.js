a07957840efbbd734c63d55be03291baa83940f0
wangcong <dciwang@163.com>
2020-08-28 00:08:23 +0800

    书籍购物处案例

---
+++ 7.书籍购物车案例/main.js
@@ -0,0 +1,69 @@
+const app=new Vue({
+    el:'#app',
+    data:{
+        books:[
+            {
+                id:1,
+                name:'算法导论',
+                date: '2020-9',
+                price: 29.00,
+                count: 1
+            },   {
+                id:2,
+                name:'golang',
+                date: '2020-9',
+                price: 99.00,
+                count: 1
+            },   {
+                id:3,
+                name:'java',
+                date: '2020-9',
+                price: 89.00,
+                count: 1
+            },   {
+                id:4,
+                name:'c',
+                date: '2020-9',
+                price: 69.00,
+                count: 1
+            },   {
+                id:5,
+                name:'c++',
+                date: '2020-9',
+                price: 99.00,
+                count: 1
+            },   {
+                id:6,
+                name:'vue',
+                date: '2020-9',
+                price: 159.00,
+                count: 1
+            }
+        ],
+
+    },methods:{
+        decrementBtn(index){
+            this.books[index].count--
+        },
+        incrementBtn(index){
+            this.books[index].count ++
+        }
+    },
+    //过滤
+    filters:{
+        showPrice(price){
+            return '¥'+price.toFixed(2)
+        }
+    },
+    computed:{
+       finalPrice(){
+           let finalPrice=0;
+           for (let argument of this.books) {
+               finalPrice+=argument.price*argument.count
+           }
+           return finalPrice;
+       },
+
+
+    }
+})
